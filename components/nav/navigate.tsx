432c1e4afc96c5491aa69ec42e2685c5a0450817
Zak Forster <zak.forster16@gmail.com>
2022-11-03 20:45:07 +0000

    fix

--- components/nav/navigate.tsx
+++ components/nav/navigate.tsx
@@ -1,7 +1,48 @@
-import styles from "../styles/Home.module.css";
+import HomeIcon from "@mui/icons-material/Home";
+import Box from "@mui/material/Box";
+import TagIcon from "@mui/icons-material/Tag";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import Button from "@mui/material/Button";
 
 const Navigate = () => {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <HomeIcon sx={{ color: "white" }} />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <TagIcon sx={{ color: "white" }} />
+            </ListItemIcon>
+            <ListItemText primary="Explore" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <NotificationsIcon sx={{ color: "white" }} />
+            </ListItemIcon>
+            <ListItemText primary="Notifications" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Button variant="contained">New Tweet</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Navigate;
