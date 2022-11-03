71504ee63ff8be8a173f2c3740571cc548d14495
charliemday <45036245+charliemday@users.noreply.github.com>
2022-11-03 21:03:36 +0000

    Added list tweets

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
+import { useEffect, useState } from "react";
 import Navigate from "../components/nav/navigate";
 
 const BASE_URL = "https://twitter.fly.dev";
 
 export default function Home() {
+  const [tweets, setTweets] = useState([]);
+
   const fetchTweets = async () => {
-    const response = await fetch("/tweets/");
+    const response = await fetch(`${BASE_URL}/tweets/`)
+      .then((r) => r.json())
+      .then((r) => setTweets(r));
   };
 
-  const createTweets = async (body: {
-    user: number;
-    content: string;
-  }) => {
+  const createTweets = async (body: { user: number; content: string }) => {
     await fetch("/tweets/", {
       method: "POST",
       body: JSON.stringify(body),
@@ -28,16 +30,36 @@ export default function Home() {
   };
 
   const getUsers = async () => {
-    const response = await fetch('/users/')
+    const response = await fetch("/users/");
   };
 
   const fetchUser = async (id: number) => {
-    const response = await fetch(`/user/${id}`)
+    const response = await fetch(`/user/${id}`);
   };
+
+  useEffect(() => {
+    fetchTweets();
+  }, []);
+
+  console.log(tweets);
 
   return (
     <div>
       <Navigate />
+      <div
+        style={{
+          position: "absolute",
+          top: 10,
+          left: 300,
+        }}
+      >
+        {tweets.map((tweet, key) => (
+          <div key={key}>
+            {/* @ts-ignore */}
+            <p>{tweet.content}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
